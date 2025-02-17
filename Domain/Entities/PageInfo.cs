3aa36b28936f9e22eb8fd1c4e47bd5bdf8717876
Clach-dev <yuhkno.siarhei@gmail.com>
2025-02-18 02:46:24 +0300

    Create repositories

---
+++ Domain/Entities/PageInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class PageInfo
+{
+    public int PageNumber { get; set; } = 1;
+    
+    public int PageSize { get; set; } = 10;
+}
---
+++ Infrastructure/Data/Repositories/OrderItemRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.IRepositories;
+
+namespace Infrastructure.Data.Repositories;
+
+public class OrderItemRepository(ShopDbContext context) : BaseRepository<OrderItem>(context), IOrderItemRepository
+{
+}
