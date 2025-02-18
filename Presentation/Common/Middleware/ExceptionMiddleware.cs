ce1ed16ec7903e288722b52c772c702654b63cda
Clach-dev <yuhkno.siarhei@gmail.com>
2025-02-18 15:14:19 +0300

    add middlewares

---
+++ Presentation/Common/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Utils;
+using Newtonsoft.Json;
+
+namespace Presentation.Common.Middleware;
+
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception occurred.");
+            
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/json";
+        
+        return context.Response.WriteAsync(JsonConvert
+            .SerializeObject(ResultBuilder.InternalServerErrorResult<byte?>(exception.Message)));
+    }
+}
---
+++ Presentation/Common/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace Presentation.Common.Middleware;
+
+public class LoggingMiddleware(
+    RequestDelegate next,
+    ILogger<LoggingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        LogRequest(context.Request);
+
+        await next(context);
+
+        LogResponse(context.Response);
+    }
+
+    private void LogRequest(HttpRequest request)
+    {
+        logger.LogInformation("Incuming Request: Method: {Method}, Path: {Path}", request.Method, request.Path);
+    }
+
+    private void LogResponse(HttpResponse response)
+    {
+        logger.LogInformation("Outgoing Response: Status Code: {StatusCode}", response.StatusCode);
+    }
+}
