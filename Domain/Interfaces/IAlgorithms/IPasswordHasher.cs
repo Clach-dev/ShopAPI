509e5834f86e09dfa004fb9ccd46134a01a07ae4
Clach-dev <yuhkno.siarhei@gmail.com>
2025-02-18 04:45:58 +0300

    add password hasher interface

---
+++ Domain/Interfaces/IAlgorithms/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Interfaces.IAlgorithms;
+
+public interface IPasswordHasher
+{
+    string HashPassword(string password);
+    
+    bool VerifyHashedPassword(string hashedPassword, string password);
+}
