9b7b438ffd8d8939845f5bb0d9d3ce9f7826ced0
Clach-dev <yuhkno.siarhei@gmail.com>
2025-02-18 05:42:54 +0300

    add page info validator

---
+++ Presentation/Common/Validators/PageInfoDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Dtos;
+using FluentValidation;
+
+namespace Presentation.Common.Validators;
+
+public class PageInfoDtoValidator : AbstractValidator<PageInfoDto>
+{
+    public PageInfoDtoValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .PageSettingsRule();
+
+        RuleFor(x => x.PageSize)
+            .PageSettingsRule();
+    }
+}
---
+++ Presentation/Common/Validators/ValidationRules.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Dtos;
+using FluentValidation;
+
+namespace Presentation.Common.Validators;
+
+public static class ValidationRules
+{
+    public static IRuleBuilder<T, int> PageSettingsRule<T>(this IRuleBuilder<T, int> ruleBuilder)
+    {
+        return ruleBuilder
+            .NotNull().WithMessage("Page settings cannot be null.")
+            .Must(x => x > 0).WithMessage("Page number must be greater than 0.");
+    }
+}
