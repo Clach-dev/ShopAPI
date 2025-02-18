dbff8e78c57cabd05aece4b99de883ea1cda8b88
Clach-dev <yuhkno.siarhei@gmail.com>
2025-02-18 05:43:07 +0300

    add page info mapping profile

---
+++ Application/Common/MappingProfiles/PageInfoMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.MappingProfiles;
+
+public class PageInfoMappingProfile : Profile
+{
+    public PageInfoMappingProfile()
+    {
+        CreateMap<PageInfoDto, PageInfo>()
+            .ForMember(dest => dest.PageNumber, opt => opt.MapFrom(src => src.PageNumber))
+            .ForMember(dest => dest.PageSize, opt => opt.MapFrom(src => src.PageSize));
+    }
+}
