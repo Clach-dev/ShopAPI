a221134c7b420d41a361d264b65b074e38c147aa
RomjanSmoll <roma.smolik00@gmail.com>
2025-03-02 23:13:55 +0300

    Orders tests

--- Tests/OrdersControllerTests.cs
+++ Tests/OrdersControllerTests.cs
@@ -1,5 +1,7 @@
 using System.Net;
+using Application.Common.Dtos;
 using Application.Common.Dtos.Order;
+using Application.Common.Dtos.User;
 using Application.Common.MappingProfiles.OrderProfiles;
 using Application.Common.Utils;
 using Application.UseCases.OrderCases.Commands.CreateOrderCase;
@@ -87,5 +89,71 @@ public class OrdersControllerTests
         
         result.Value.Should().BeEquivalentTo(createOrderDto, options => options
             .Excluding(x => x.UserId));
+    }
+    
+    [Fact]
+    public async Task CreateOrder_OrderWithNotExistingUserId_ReturnsConflict()
+    {
+        // Arrange
+        var user = _fixture.Build<User>().Without(u => u.Orders).Create();
+        
+        _shopDbContext.Users.Add(user);
+        await _shopDbContext.SaveChangesAsync();
+        
+        var createOrderDto = _fixture.Build<CreateOrderDto>()
+            .With(x => x.UserId, user.Id)
+            .Create();
+        
+        // Act
+        var act = await _ordersController.CreateOrder(createOrderDto, default);
+        
+        // Assert
+        act.Should().BeOfType<ObjectResult>();
+        
+        var result = act.As<ObjectResult>().Value.As<Result<ReadOrderDto>>();
+        
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+        result.StatusCode.Should().Be(HttpStatusCode.Conflict);
+        result.Value.Should().BeNull();
+        result.Errors.Should().Contain(ErrorMessages.UserIdNotFoundError);
+    }
+    
+    [Fact]
+    public async Task GetAllOrders_ReturnsOk()
+    {
+        // Arrange
+        var user = _fixture.Build<User>().Without(u => u.Orders).Create();
+
+        var orders = _fixture.Build<Order>().With(x => x.UserId, user.Id).Without(x => x.OrderItems).Without(x => x.User).CreateMany(3).ToList();
+        await _shopDbContext.Users.AddAsync(user);
+        await _shopDbContext.Orders.AddRangeAsync(orders);
+        await _shopDbContext.SaveChangesAsync();
+
+        // Act
+        var act = await _ordersController.GetAllOrders(new PageInfoDto(),default);
+        
+        // Assert
+        act.Should().BeOfType<ObjectResult>();
+        
+        var result = act.As<ObjectResult>().Value.As<Result<ReadOrdersDto>>();
+        
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        result.Value.Should().NotBeNull();
+        result.Errors.Should().BeNull();
+        
+        result.Value.Orders.Should().HaveCount(3);
+        
+        result.Value.Orders.Should().BeEquivalentTo(orders, options => options
+            .Excluding(x => x.TotalPrice)
+            .Excluding(x => x.Status)
+            .Excluding(x => x.DeliveryDate)
+            .Excluding(x => x.UserId)
+            .Excluding(x => x.OrderItems)
+            .Excluding(x => x.User));
+        
+        result.Value.TotalCount.Should().Be(3);
     }
 }
